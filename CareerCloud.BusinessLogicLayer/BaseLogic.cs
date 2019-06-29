a33f652937f25f7496231d8967d51b23313d93e5
Neha Parekh <nlimbachiya207@gmail.com>
2019-06-29 12:36:45 -0400

    Created BaseLogic

--- CareerCloud.BusinessLogicLayer/BaseLogic.cs
+++ CareerCloud.BusinessLogicLayer/BaseLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,51 @@ using System.Threading.Tasks;
 
 namespace CareerCloud.BusinessLogicLayer
 {
-    class BaseLogic
+    public abstract class BaseLogic<TPoco>
+        where TPoco : IPoco
     {
+        protected IDataRepository<TPoco> _repository;
+        public BaseLogic(IDataRepository<TPoco> repository)
+        {
+            _repository = repository;
+        }
+
+        protected virtual void Verify(TPoco[] pocos)
+        {
+            return;
+        }
+
+        public virtual TPoco Get(Guid id)
+        {
+            return _repository.GetSingle(c => c.Id == id);
+        }
+
+        public virtual List<TPoco> GetAll()
+        {
+            return _repository.GetAll().ToList();
+        }
+
+        public virtual void Add(TPoco[] pocos)
+        {
+            foreach (TPoco poco in pocos)
+            {
+                if (poco.Id == Guid.Empty)
+                {
+                    poco.Id = Guid.NewGuid();
+                }
+            }
+
+            _repository.Add(pocos);
+        }
+
+        public virtual void Update(TPoco[] pocos)
+        {
+            _repository.Update(pocos);
+        }
+
+        public void Delete(TPoco[] pocos)
+        {
+            _repository.Remove(pocos);
+        }
     }
 }
