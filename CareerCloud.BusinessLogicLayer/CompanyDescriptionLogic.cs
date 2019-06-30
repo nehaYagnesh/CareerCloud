5581fbc0c315feca11406627146d7769392e12b1
Neha Parekh <nlimbachiya207@gmail.com>
2019-06-29 23:57:49 -0400

    CompanyDescriptionLogic : Added validation rules

--- CareerCloud.BusinessLogicLayer/CompanyDescriptionLogic.cs
+++ CareerCloud.BusinessLogicLayer/CompanyDescriptionLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,44 @@ using System.Threading.Tasks;
 
 namespace CareerCloud.BusinessLogicLayer
 {
-    class CompanyDescriptionLogic
+    public class CompanyDescriptionLogic : BaseLogic<CompanyDescriptionPoco>
     {
+        public CompanyDescriptionLogic(IDataRepository<CompanyDescriptionPoco> repository) : base(repository)
+        {
+           
+        }
+
+        public override void Add(CompanyDescriptionPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyDescriptionPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(CompanyDescriptionPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach(var poco in pocos)
+            {
+                if(poco.CompanyDescription.Length < 2)
+                {
+                    exceptions.Add(new ValidationException(107, $"CompanyDescription of {poco.Id} must be greater than 2 characters"));
+                }
+                if (poco.CompanyName.Length < 2)
+                {
+                    exceptions.Add(new ValidationException(107, $"CompanyName of {poco.Id} must be greater than 2 characters"));
+                }
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
     }
 }
