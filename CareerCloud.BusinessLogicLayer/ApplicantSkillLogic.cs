e5d3578937ad88f5b975d0a5dace86442e2ab666
Neha Parekh <nlimbachiya207@gmail.com>
2019-06-29 20:51:38 -0400

    ApplicantSkillLogic -> Added validation rules

--- CareerCloud.BusinessLogicLayer/ApplicantSkillLogic.cs
+++ CareerCloud.BusinessLogicLayer/ApplicantSkillLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,52 @@ using System.Threading.Tasks;
 
 namespace CareerCloud.BusinessLogicLayer
 {
-    class ApplicantSkillLogic
+    public class ApplicantSkillLogic : BaseLogic<ApplicantSkillPoco>
     {
+        public ApplicantSkillLogic(IDataRepository<ApplicantSkillPoco> repository) : base(repository)
+        {
+
+        }
+
+        public override void Add(ApplicantSkillPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantSkillPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(ApplicantSkillPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach(var poco in pocos)
+            {
+                if(poco.StartMonth > 12)
+                {
+                    exceptions.Add(new ValidationException(101, $"Start Date for {poco.Id} cannot be greater than 12"));
+                }
+                if(poco.EndMonth> 12)
+                {
+                    exceptions.Add(new ValidationException(102, $"End Date for {poco.Id} cannot be greater than 12"));
+                }
+                if(poco.StartYear < 1900)
+                {
+                    exceptions.Add(new ValidationException(103,$"Start Year for {poco.Id} cannot be less than 1900"));
+                }
+                if (poco.EndYear < poco.StartYear)
+                {
+                    exceptions.Add(new ValidationException(103, $"End Year for {poco.Id} cannot be less than StartYear"));
+                }
+            }
+
+            if(exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
     }
 }
