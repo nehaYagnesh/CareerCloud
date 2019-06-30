30e4b384f253bf6472bcda4d3252743e67c70e73
Neha Parekh <nlimbachiya207@gmail.com>
2019-06-30 00:21:42 -0400

    CompanyJobEducationLogic -> Added validation rules

--- CareerCloud.BusinessLogicLayer/CompanyJobEducationLogic.cs
+++ CareerCloud.BusinessLogicLayer/CompanyJobEducationLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,43 @@ using System.Threading.Tasks;
 
 namespace CareerCloud.BusinessLogicLayer
 {
-    class CompanyJobEducationLogic
+    public class CompanyJobEducationLogic : BaseLogic<CompanyJobEducationPoco>
     {
+        public CompanyJobEducationLogic(IDataRepository<CompanyJobEducationPoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(CompanyJobEducationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyJobEducationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(CompanyJobEducationPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (var poco in pocos)
+            {
+                if ((poco.Major.Length < 2) || (string.IsNullOrEmpty(poco.Major)))
+                {
+                    exceptions.Add(new ValidationException(200, $"Major of {poco.Id} must be at least 2 characters"));
+                }
+                if (poco.Importance < 0)
+                {
+                    exceptions.Add(new ValidationException(201, $"Importance of {poco.Id} cannot be less than 0"));
+                }
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
     }
 }
