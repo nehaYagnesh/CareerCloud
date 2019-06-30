8e9aee7514308ea3e6f1f85a4909bcbcbeb5b517
Neha Parekh <nlimbachiya207@gmail.com>
2019-06-29 20:14:37 -0400

    Added validation logic for ApplicantJobApplicationLogic

--- CareerCloud.BusinessLogicLayer/ApplicantJobApplicationLogic.cs
+++ CareerCloud.BusinessLogicLayer/ApplicantJobApplicationLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace CareerCloud.BusinessLogicLayer
 {
-    class ApplicantJobApplicationLogic
+    public class ApplicantJobApplicationLogic : BaseLogic<ApplicantJobApplicationPoco>
     {
+        public ApplicantJobApplicationLogic(IDataRepository<ApplicantJobApplicationPoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(ApplicantJobApplicationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantJobApplicationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(ApplicantJobApplicationPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+
+            foreach (var poco in pocos)
+            {
+                if (poco.ApplicationDate > DateTime.Now)
+                {
+                    exceptions.Add(new ValidationException(110, $"Application Date for {poco.Id} cannot be greater than today"));
+                }
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+
+        }
     }
 }
