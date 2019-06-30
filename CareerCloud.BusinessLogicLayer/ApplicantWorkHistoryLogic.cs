7a10cfbf997bf6bd7536f6ea41f5ef0051024137
Neha Parekh <nlimbachiya207@gmail.com>
2019-06-29 23:32:30 -0400

    ApplicantWorkHistoryLogic -> Added validation rules

--- CareerCloud.BusinessLogicLayer/ApplicantWorkHistoryLogic.cs
+++ CareerCloud.BusinessLogicLayer/ApplicantWorkHistoryLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,40 @@ using System.Threading.Tasks;
 
 namespace CareerCloud.BusinessLogicLayer
 {
-    class ApplicantWorkHistoryLogic
+    public class ApplicantWorkHistoryLogic : BaseLogic<ApplicantWorkHistoryPoco>
     {
+        public ApplicantWorkHistoryLogic(IDataRepository<ApplicantWorkHistoryPoco> repository) : base(repository)
+        {
+
+        }
+
+        public override void Add(ApplicantWorkHistoryPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantWorkHistoryPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(ApplicantWorkHistoryPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach(var poco in pocos)
+            {
+                if(poco.CompanyName.Length > 2)
+                {
+                    exceptions.Add(new ValidationException(105, $"Company Name for {poco.Id} must be greater then 2 characters"));
+                }
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
     }
 }
