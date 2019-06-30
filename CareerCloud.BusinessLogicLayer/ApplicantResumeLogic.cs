ec0a035915946c8e308b684f20b727641bc08775
Neha Parekh <nlimbachiya207@gmail.com>
2019-06-29 20:37:50 -0400

    ApplicantResumeLogic ->Added validation rules

--- CareerCloud.BusinessLogicLayer/ApplicantResumeLogic.cs
+++ CareerCloud.BusinessLogicLayer/ApplicantResumeLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,40 @@ using System.Threading.Tasks;
 
 namespace CareerCloud.BusinessLogicLayer
 {
-    class ApplicantResumeLogic
+    public class ApplicantResumeLogic : BaseLogic<ApplicantResumePoco>
     {
+        public ApplicantResumeLogic(IDataRepository<ApplicantResumePoco> repository) : base(repository)
+        {
+
+        }
+
+        public override void Add(ApplicantResumePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantResumePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(ApplicantResumePoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach(var poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.Resume))
+                {
+                    exceptions.Add(new ValidationException(113, $"Resume of {poco.Id} cannot be empty"));
+                }
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
     }
 }
