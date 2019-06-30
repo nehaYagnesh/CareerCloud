091e55615eb34b7c9a70b9f4f7dd85f3bc9f6425
Neha Parekh <nlimbachiya207@gmail.com>
2019-06-29 20:26:19 -0400

    ApplicantProfileLogic : Added validation rules

--- CareerCloud.BusinessLogicLayer/ApplicantProfileLogic.cs
+++ CareerCloud.BusinessLogicLayer/ApplicantProfileLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,44 @@ using System.Threading.Tasks;
 
 namespace CareerCloud.BusinessLogicLayer
 {
-    class ApplicantProfileLogic
+    public class ApplicantProfileLogic : BaseLogic<ApplicantProfilePoco>
     {
+        public ApplicantProfileLogic(IDataRepository<ApplicantProfilePoco> repository) : base(repository)
+        {
+
+        }
+
+        public override void Add(ApplicantProfilePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantProfilePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(ApplicantProfilePoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach(var poco in pocos)
+            {
+                if (poco.CurrentSalary <0)
+                {
+                    exceptions.Add(new ValidationException(111, $"Current Salary of {poco.Id} cannot be negative"));
+                }
+                if (poco.CurrentRate <0)
+                {
+                    exceptions.Add(new ValidationException(112, $"CurrentRate of {poco.Id} cannot be negative"));
+                }
+            }
+
+            if(exceptions.Count >0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
     }
 }
