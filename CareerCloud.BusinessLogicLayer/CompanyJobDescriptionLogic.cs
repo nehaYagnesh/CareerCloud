232090784032d65c81f12b906923dad489f0098b
Neha Parekh <nlimbachiya207@gmail.com>
2019-06-30 00:07:54 -0400

    CompanyJobDescriptionLogic -> Added validation rules

--- CareerCloud.BusinessLogicLayer/CompanyJobDescriptionLogic.cs
+++ CareerCloud.BusinessLogicLayer/CompanyJobDescriptionLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,42 @@ using System.Threading.Tasks;
 
 namespace CareerCloud.BusinessLogicLayer
 {
-    class CompanyJobDescriptionLogic
+    public class CompanyJobDescriptionLogic : BaseLogic<CompanyJobDescriptionPoco>
     {
+        public CompanyJobDescriptionLogic(IDataRepository<CompanyJobDescriptionPoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(CompanyJobDescriptionPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyJobDescriptionPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(CompanyJobDescriptionPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (var poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.JobName))
+                {
+                    exceptions.Add(new ValidationException(300, $"JobName of {poco.Id} cannot be empty"));
+                }
+                if (string.IsNullOrEmpty(poco.JobDescriptions))
+                {
+                    exceptions.Add(new ValidationException(301, $"JobDescriptions of {poco.Id} cannot be empty"));
+                }
+            }
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
     }
 }
