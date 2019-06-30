1258b96d7f70e3ebff76b46a012fd88fa183536f
Neha Parekh <nlimbachiya207@gmail.com>
2019-06-30 00:34:13 -0400

    CompanyJobSkillLogic -> Added validation rules

--- CareerCloud.BusinessLogicLayer/CompanyJobSkillLogic.cs
+++ CareerCloud.BusinessLogicLayer/CompanyJobSkillLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,38 @@ using System.Threading.Tasks;
 
 namespace CareerCloud.BusinessLogicLayer
 {
-    class CompanyJobSkillLogic
+    public class CompanyJobSkillLogic : BaseLogic<CompanyJobSkillPoco>
     {
+        public CompanyJobSkillLogic(IDataRepository<CompanyJobSkillPoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(CompanyJobSkillPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyJobSkillPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(CompanyJobSkillPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (var poco in pocos)
+            {
+                if (poco.Importance < 0)
+                {
+                    exceptions.Add(new ValidationException(400, $"Importance for {poco.Id} cannot be less than 0"));
+                }
+            }
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
     }
 }
